2974862d7f685943c40d59dde8d214fcdba9d84a
wdminarul <wdminarul100@gmail.com>
2024-07-31 18:28:59 +0600

    even-odd-number

---
+++ even.js
@@ -0,0 +1,19 @@
+/*
+1. Show even numbers with while loop
+*/ 
+
+var evenNumber = 2;
+while(evenNumber <= 100){
+    // console.log(evenNumber);
+    evenNumber +=2;
+}
+
+/*
+1. Show odd numbers with while loop
+*/ 
+
+var oddNumber = 1;
+while(oddNumber <= 100){
+    console.log(oddNumber);
+    oddNumber+=2;
+}
---
+++ while-loop.js
@@ -0,0 +1,22 @@
+
+/*
+1. loop variable
+2. condition setup in while
+3. output show on loop body
+4. change the value of variable // ++ -- 
+*/ 
+var dollarhave = 0;
+while(dollarhave <9){
+    // console.log('Money given')
+    // console.log(dollarhave)
+    dollarhave++;
+    
+}
+
+
+var number = 1;
+
+while(number <=10){
+    console.log(number);
+    number++
+}
